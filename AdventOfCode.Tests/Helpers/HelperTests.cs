432192cca6f1f2b5b2cc050c0eb821901e1ca592
Mikko Mattila <mikkkoo93@hotmail.com>
2023-12-02 22:56:26 +0200

    Day1 finished.

--- AdventOfCode.Tests/Helpers/HelperTests.cs
+++ AdventOfCode.Tests/Helpers/HelperTests.cs
@@ -8,9 +8,12 @@ public class HelperTests
     private static readonly string _fileName = "Day2TestInput.txt";
 
     [Fact]
-    public void ReadInputLines_ReturnsCorrectAmountOfRows()
+    public void ReadInputLines_Returns_CorrectAmountOfRows()
     {
-        var input = Helper.ReadInputLines(_inputFolder, _fileName);
-        input.Length.Should().Be(5);
+        var input1 = Helper.ReadInputLines(_inputFolder, "Day1TestInput2.txt");
+        var input2 = Helper.ReadInputLines(_inputFolder, "Day2TestInput.txt");
+
+        input1.Length.Should().Be(7);
+        input2.Length.Should().Be(5);
     }
 }
--- AdventOfCode/Solutions2023/Day1.cs
+++ AdventOfCode/Solutions2023/Day1.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode.Interfaces;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Solutions2023;
 
@@ -11,36 +12,64 @@ public class Day1 : IDay
     private static readonly string _inputFolder = "Solutions2023";
     private static readonly string _inputFileName = "Day1Input.txt";
 
+    private static readonly Dictionary<string, string> _numericMapping = new() {
+        { "one", "1" },
+        { "two", "2" },
+        { "three", "3" },
+        { "four", "4" },
+        { "five", "5" },
+        { "six", "6" },
+        { "seven", "7" },
+        { "eight", "8" },
+        { "nine", "9" },
+        { "1", "1" },
+        { "2", "2" },
+        { "3", "3" },
+        { "4", "4" },
+        { "5", "5" },
+        { "6", "6" },
+        { "7", "7" },
+        { "8", "8" },
+        { "9", "9" },
+    };
+
     public void Run()
     {
         var input = Helper.ReadInputLines(_inputFolder, _inputFileName);
-        var digits = ParseCombinedDigits(input);
+        var firstAnswer = ParseCombinedDigits(input).Sum();
+        var secondAnswer = ParseCombinedDigits(input, true).Sum();
 
         Console.WriteLine(
-            $"2023-01 answer 1: {GetFirstAnswer(digits)}"
+            $"2023-01 answer 1: {firstAnswer}\n" +
+            $"2023-01 answer 2: {secondAnswer}"
         );
     }
+    
+    public static string AggregateNumbers(string input, bool includeWords)
+    {
+        var pattern = includeWords 
+            ? @"(?i)(one|two|three|four|five|six|seven|eight|nine|\d{1})" 
+            : @"(\d{1})";
+        
+        var matches = Regex.Matches(input, pattern);
 
-    public static List<int> ParseCombinedDigits(string[] input)
+        var aggregate = matches
+            .Aggregate(string.Empty, (output, next) => output + _numericMapping[next.Groups[1].Value]);
+
+        return aggregate;
+    }
+
+    public static List<int> ParseCombinedDigits(string[] input, bool includeWords = false)
         => input.Select(x =>
             {
-                int firstDigit = 0, secondDigit = 0;
-
-                var numeric = new string(x.Where(char.IsDigit).ToArray());
-                if (!string.IsNullOrEmpty(numeric))
-                {
-                    firstDigit = int.Parse($"{numeric[0]}");
-
-                    var secondIndex = numeric.Length - 1;
-                    secondIndex = secondIndex > 0 ? secondIndex : 0;
-
-                    secondDigit = int.Parse($"{numeric[secondIndex]}");
-                }
-                return firstDigit * 10 + secondDigit;
+                var numbersAggregate = AggregateNumbers(x, includeWords);
+                var firstDigit = numbersAggregate.Substring(0, 1);
+                var secondDigit = numbersAggregate.Substring(numbersAggregate.Length - 1, 1);
+                return CombineNumbers(firstDigit, secondDigit);
             }
         ).ToList() 
         ?? new List<int>();
 
-    public static int GetFirstAnswer(List<int> combinedNumber)
-        => combinedNumber.Sum();
+    private static int CombineNumbers(string firstDigit, string secondDigit)
+        => int.Parse(firstDigit) * 10 + int.Parse(secondDigit);
 }
