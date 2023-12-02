7f3121fcffc56bd646355c15d1b69d112902a69f
Mikko Mattila <mikkkoo93@hotmail.com>
2023-12-02 16:33:47 +0200

    Initial setup.

---
+++ AdventOfCode/Interfaces/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace AdventOfCode.Interfaces;
+
+public interface IDay
+{
+    public void Run();
+}
---
+++ AdventOfCode/Solutions2023/Day2/Day2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AdventOfCode.Interfaces;
+
+namespace AdventOfCode.Solutions2023.Day2;
+
+public class Day2 : IDay
+{
+    public void Run()
+    {
+        Console.WriteLine("Calculating result for day2...");
+
+    }
+}
+
