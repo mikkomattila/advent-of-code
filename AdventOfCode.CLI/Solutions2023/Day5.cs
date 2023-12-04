f881f323ac58ddfbe350754e628f342e28a9e88b
Mikko Mattila <mikkkoo93@hotmail.com>
2023-12-04 23:58:30 +0200

    Added day 5 files.

---
+++ AdventOfCode.CLI/Solutions2023/Day5.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode.Classes;
+using AdventOfCode.Interfaces;
+
+namespace AdventOfCode.CLI.Solutions2023;
+
+/// <summary>
+/// Solution for day 5 of 2023.
+/// Instructions: https://adventofcode.com/2023/day/5.
+/// </summary>
+public class Day5 : IDay
+{ 
+    public DayResult GetResultForDay()
+    {
+        var input = Helper.ReadInputLines("5");
+        var firstAnswer = GetFirstAnswer(input);
+        var secondAnswer = GetSecondAnswer(input);
+
+        return new DayResult($"{firstAnswer}", $"{secondAnswer}");
+    }
+
+    public static int GetFirstAnswer(string[] input)
+    {
+
+
+        return 0;
+    }
+
+    public static int GetSecondAnswer(string[] input)
+    {
+
+
+        return 0;
+    }
+}
---
+++ AdventOfCode.Tests/Solutions2023/Day5Tests.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode.Solutions2023;
+using FluentAssertions;
+
+namespace AdventOfCode.Tests.Solutions2023;
+
+public class Day5Tests
+{
+
+}
