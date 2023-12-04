0def1ce854a4e1cadfc4150ac3c75c8f8047e04f
Mikko Mattila <mikkkoo93@hotmail.com>
2023-12-04 17:23:29 +0200

    Remove d4 part 2 for now.

--- AdventOfCode/Solutions2023/Day4.cs
+++ AdventOfCode/Solutions2023/Day4.cs
@@ -13,9 +13,8 @@ public class Day4 : IDay
     {
         var input = Helper.ReadInputLines("Day4Input.txt");
         var firstAnswer = GetFirstAnswer(input);
-        var secondAnswer = GetSecondAnswer(input);
 
-        return new DayResult(firstAnswer.ToString(), "");
+        return new DayResult(firstAnswer.ToString(), string.Empty);
     }
 
     public static double GetFirstAnswer(string[] cards)
@@ -41,41 +40,6 @@ public class Day4 : IDay
         }
 
         return totalPoints;
-    }
-
-    public static double GetSecondAnswer(string[] input)
-    {
-        var result = 0;
-
-        var cards = input
-            .Select((value, i) => (value, i));
-
-        foreach (var (card, cardIndex) in cards)
-        {
-            var cardParts = card.Split('|');
-            var winningNumbers = ParseNumbers(cardParts[0].Split(':')[1]);
-            var resultNumbers = ParseNumbers(cardParts[1]);
-
-            var matchingNumbers = winningNumbers.Intersect(resultNumbers).ToList();
-
-            var cardCopies = string.Empty;
-
-            foreach (var match in matchingNumbers)
-            {
-                var nextCardIndex = cardIndex + 1;
-                if (nextCardIndex > cards.Count())
-                    break;
-
-                var nextCard = cards.ElementAt(cardIndex + 1).value;
-                Console.WriteLine("current" + card);
-                Console.WriteLine("nextcard"+nextCard);
-
-            }
-
-        }
-
-
-        return 0;
     }
 
     private static List<int> ParseNumbers(string input)
