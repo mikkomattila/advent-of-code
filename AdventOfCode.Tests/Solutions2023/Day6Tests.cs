aaaa8a620691d07c9fa28265e74514b8485d6106
Mikko Mattila <mikkkoo93@hotmail.com>
2023-12-06 11:38:56 +0200

    Day 6 first answer.

---
+++ AdventOfCode.Tests/Solutions2023/Day6Tests.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode.Solutions2023;
+using FluentAssertions;
+
+namespace AdventOfCode.Tests.Solutions2023;
+
+public class Day6Tests
+{
+    [Fact]
+    public void GetFirstAnswer_Returns_CorrectAnswer()
+    {
+        var input = Helper.ReadInputLines("6");
+        var race = Day6.ParseRaceInput(input);
+        var firstAnswer = Day6.GetFirstAnswer(race);
+
+        firstAnswer.Should().Be(288);
+    }
+}
